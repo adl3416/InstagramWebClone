d6207d7c24f7f8bb7f65ab78ebf80bc8ba2e7b66
adl <makara3416@gmail.com>
2024-06-16 08:27:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 
 
 function App() {
+
+
   return (
-    <div className="App">
-     <h1> Clone App</h1>
+
+    
+    <div  className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
+    
+     <div>
+     <img
+								 src="https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png" alt=""/>
+     </div>
     </div>
   );
 }
