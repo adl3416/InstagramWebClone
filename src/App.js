bc833051a02075124794747d8aebf8c71f27d8d4
adl <makara3416@gmail.com>
2024-06-17 13:33:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
     <div  className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
     
      <div>
-     <img
-								 src="https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png" alt=""/>
+     <img className="w-[380px] h-[580px]" src="https://www.instagram.com/static/images/homepage/phones/home-phones-2x.png/cbc7174b4f05.png" alt=""/>
      </div>
     </div>
   );
