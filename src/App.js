a34c3dc4870936805b6f9fb83d81b2ca2a3c0e27
adl <makara3416@gmail.com>
2024-06-18 13:27:49 +0200

    v

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     
     <div  className="h-full w-full flex flex-wrap overflow-auto items-center gap-x-8 justify-center">
           
-     <div className=" w-[380px] h-[581px] bg-red-300  bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_ -46px]">                               {/*logo-pattern i tailvind.config de ayarladik. */}
+     <div className=" w-[380px] h-[581px] bg-red-300  bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">                               {/*logo-pattern i tailvind.config de ayarladik. resmin adresi oradaverildi */}
     
      </div>
     </div>
