a9838c13fc80bc2c26c530d932580969fa3e8ff8
adl <makara3416@gmail.com>
2024-06-20 17:45:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ useEffect(() => {
 						<img className="h-[51px]"
 								 src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png" alt=""/>
 					</a>
-        <form>
+        <form className="grid gap-y-1">
           <label className="block relative"> 
             <input type="text" required={true} className=" bg-zinc-50 px-2 border  rounded-sm outline-none text-xs focus:border-gray-400 w-full h-[38px] valid:pt-[10px] peer"/>
             <small className="absolute top-1/2 left-[9px] cursor-text pointer-events-none text-xs text-gray-400 -translate-y-1/2 transition-all peer-valid:text-[10px] peer-valid:top-2.5" >Phone number, username or email</small>
